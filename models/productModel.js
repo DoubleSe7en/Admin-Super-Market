31affcb216a9c3cb09f0a57ef4bce106f0f03f89
= <=>
2019-06-10 14:32:08 +0700

    update 10/06/2019

--- models/productModel.js
+++ models/productModel.js
@@ -3,19 +3,17 @@ var Schema = mongoose.Schema;
 
 var product = new Schema({
   name: String,
-  imgSrc: String,
-  count: Number,
   price: Number,
   sale: Number,
+  unit: String,
   category: String,
+  count: Number,
+  numberSold: Number,
   color: String,
   size: String,
   rate: Number,
   info: String,
-  uri: String,
-  row1: String,
-  row2: String,
-  row3: String
+  imgSrc: String
 },{collection: 'products'});
 
 module.exports = mongoose.model('products',product);
